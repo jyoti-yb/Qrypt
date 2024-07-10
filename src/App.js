530b2b11bdfb6b75d93aaea90e60bc19323d756a
Priyal Singh <priyalrajput595@gmail.com>
2024-07-10 15:08:51 +0530

    Updated App.js to add Chat page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './Signup';
+import Chat from './Chat';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
       <nav>
         <ul>
           <li>
@@ -21,9 +27,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
+      
+      <footer>
+        <Link to="/chat">Go to Chat</Link>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
