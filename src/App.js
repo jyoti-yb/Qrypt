62f6353ad15d10290da69ed3caab64484e226500
Priyal Singh <priyalrajput595@gmail.com>
2024-07-11 20:16:25 +0530

    Imported navbar in App.js and updated it

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Login from './Login';
-import SignUp from './Signup';
-import Chat from './Chat';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 import './App.css';
-import logo from './logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/" element={<Navbar />} />
       </Routes>
-      
-      <footer>
-        <Link to="/chat">Go to Chat</Link>
-      </footer>
-    </div>
+    </Router>
   );
 }
 
