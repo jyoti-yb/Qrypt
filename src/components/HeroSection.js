cf7ca054f20e189697cbf322a6255877d06d21f2
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 18:55:46 +0530

    Edited the text and added breaks

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,9 +12,9 @@ function HeroSection() {
         <h3>Experience the Future of Secure Texting</h3>
         <p>
   Protect your communications from the threats of tomorrow with Qrypt's cutting-edge, web-based texting platform.<br />
-  Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure,<br />
-  even against the power of quantum computers.
+  Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure.
 </p>
+<br />
 
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
