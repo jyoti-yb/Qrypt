8d5d7a1c39c233f8226c32938291407dc3643e6c
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 21:31:11 +0530

    Added an AboutUs page with a background

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function AboutUs() {
+  return (
+    <div className="aboutus">
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
