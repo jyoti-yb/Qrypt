09478e111ea7937adf3098f50fdb21d7e3ed176f
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 17:37:51 +0530

    Added video and a welcome text to the main page

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <video src='/Videos/Main_page.mp4' autoPlay loop muted />
+        <h1>Welcome to Qrypt!</h1>
+        <h3>Experience the Future of Secure Texting</h3>
+        <p>Protect your communications from the threats of tomorrow with Qrypt's cutting-edge,web-based texting platform. Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure, even against the power of quantum computers.</p>
+      
+    </div>
+  )
+}
+
+export default HeroSection
