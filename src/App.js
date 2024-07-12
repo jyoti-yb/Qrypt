e21d3f8d6b36d1b53df0c09c9f4995287618e268
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 21:35:28 +0530

    Linked SignUp and AboutUs page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import './App.css';
 import Home from './components/pages/Home';
+import AboutUs from './components/pages/AboutUs';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={
-          <>
-          <Navbar />
-          <Home />
-          </>
-          } />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
 
