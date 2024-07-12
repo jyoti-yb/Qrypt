cdabaa7d12b8c0c535529aa54ee2786ece04fad9
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 18:24:47 +0530

    Linked the home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={
+          <>
+          <Navbar />
+          <Home />
+          </>
+          } />
+
       </Routes>
     </Router>
   );
