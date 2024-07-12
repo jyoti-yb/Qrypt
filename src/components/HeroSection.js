50483f85420d14384e567001110c123bd8ea90ef
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 18:34:09 +0530

    Added suitable breaks to enhance the readability

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,12 @@ function HeroSection() {
         <video src='/Videos/Main_page.mp4' autoPlay loop muted />
         <h1>Welcome to Qrypt!</h1>
         <h3>Experience the Future of Secure Texting</h3>
-        <p>Protect your communications from the threats of tomorrow with Qrypt's cutting-edge,web-based texting platform. Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure, even against the power of quantum computers.</p>
+        <p>
+  Protect your communications from the threats of tomorrow with Qrypt's cutting-edge, web-based texting platform.<br />
+  Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure,<br />
+  even against the power of quantum computers.
+</p>
+
       <div className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
             GET STARTED
