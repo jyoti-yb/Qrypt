e04caf2f3a7bb9be127e5c347d02144b0c48a45f
Priyal Singh <priyalrajput595@gmail.com>
2024-07-12 17:43:04 +0530

    Added a button at the main page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from './Button'
 
 function HeroSection() {
   return (
@@ -7,7 +8,12 @@ function HeroSection() {
         <h1>Welcome to Qrypt!</h1>
         <h3>Experience the Future of Secure Texting</h3>
         <p>Protect your communications from the threats of tomorrow with Qrypt's cutting-edge,web-based texting platform. Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure, even against the power of quantum computers.</p>
-      
+      <div className='hero-btns'>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+            GET STARTED
+        </Button>
+
+      </div>
     </div>
   )
 }
