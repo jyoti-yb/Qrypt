6037d05839ba61085e98fb73f10ac7769ea5692c
Priyal Singh <priyalrajput595@gmail.com>
2024-07-13 23:12:18 +0530

    Added the background video

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -5,6 +5,7 @@ import LoginRegister from '../LoginRegister';
 function SignUp() {
   return (
     <div className="sign-up">
+      <video src='/Videos/Main_page.mp4' autoPlay loop muted />
       <LoginRegister />
     </div>
   );
