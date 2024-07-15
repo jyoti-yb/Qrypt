01cdb603bbfd2373cbcec0a6cdfe6f699d73d84d
Priyal Singh <priyalrajput595@gmail.com>
2024-07-15 22:12:41 +0530

    Added introduction and overview

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,14 @@ import './About.css';
 
 const About = () => {
   return (
-    <div className="hello-container">
-      Hello
+    <div className="about-container">
+      <h1>QRYPT</h1>
+      <p>A web-based messaging application that ensures security against the future threats of quantum computers.</p>
+      <p>The main objective of this project is to learn about post-cryptographic techniques and algorithms.</p>
+      <h2>Overview</h2>
+      <p>
+        This project is a web-based messaging app that ensures security against future quantum computing threats by using advanced cryptographic techniques. It allows users to send and receive messages that are encrypted in a way that makes them very hard to intercept or decode by unauthorized parties.
+      </p>
     </div>
   );
 }
