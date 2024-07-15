895a950665096eed67a5426f568f279d1ef3c0d8
Priyal Singh <priyalrajput595@gmail.com>
2024-07-15 21:57:12 +0530

    Removed jsx file and added js file

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="hello-container">
+      Hello
+    </div>
+  );
+}
+
+export default About;
