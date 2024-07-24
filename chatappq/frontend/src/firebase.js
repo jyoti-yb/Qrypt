8fadddd72c408dfd5539fe5c602b4cfc41c77bcc
Priyal Singh <priyalrajput595@gmail.com>
2024-07-24 15:37:29 +0530

    Added the firebase.js

--- chatappq/frontend/src/firebase.js
+++ chatappq/frontend/src/firebase.js
@@ -6,14 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  
+  apiKey: "AIzaSyCxwUkBMIeUvduZZzXQHEtGJCe-Fq8TgUg",
+  authDomain: "chatapp-d3424.firebaseapp.com",
+  projectId: "chatapp-d3424",
+  storageBucket: "chatapp-d3424.appspot.com",
+  messagingSenderId: "839250672249",
+  appId: "1:839250672249:web:f2ade07a43ac2e3af2b2a8",
+  measurementId: "G-46DR0CK6RH"
 };
 
 // Initialize Firebase
