bea26099e150223456b5f825fe1192d56e94c2fd
Priyal Singh <priyalrajput595@gmail.com>
2024-07-24 22:50:58 +0530

    updated the functioning

--- chatappq/frontend/src/components/WelcomePage.js
+++ chatappq/frontend/src/components/WelcomePage.js
@@ -1,27 +1,12 @@
-// frontend/src/WelcomePage.js
 import React from 'react';
-import { auth, provider } from '../firebase';
-import { signInWithPopup } from "firebase/auth";
-
 import './WelcomePage.css';
 
-function WelcomePage({ onSubmitUsername }) {
-  const handleSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        onSubmitUsername(user.displayName);
-      })
-      .catch((error) => {
-        console.error("Error signing in with Google: ", error);
-      });
-  };
-
+function WelcomePage({ onSignIn }) {
   return (
     <div className="welcome-page">
       <div className="welcome-content">
         <h1>Welcome to the Chat App</h1>
-        <button onClick={handleSignIn}>Sign in with Google</button>
+        <button onClick={onSignIn}>Sign in with Google</button>
       </div>
     </div>
   );
