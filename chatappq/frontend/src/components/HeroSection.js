144743a21d1ef474e0662ffd287a29d18d7554c5
Priyal Singh <priyalrajput595@gmail.com>
2024-07-24 15:46:46 +0530

    Added herosection and its css

---
+++ chatappq/frontend/src/components/HeroSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <video src='/Videos/Main_page.mp4' autoPlay loop muted />
+        <h1>Welcome to Qrypt!</h1>
+        <h3>Experience the Future of Secure Texting</h3>
+        <p>
+        Protect your communications from the threats of tomorrow with Qrypt's cutting-edge, web-based texting platform.
+        </p>
+        <p>
+          Using the revolutionary post-quantum Kyber algorithm, we ensure that your messages remain private and secure.
+        </p>
+<br />
+
+      <div className='hero-btns'>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+            GET STARTED
+        </Button>
+
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
